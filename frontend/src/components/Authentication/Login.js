0358e809c73d940f11534bc2c6097bdde984f13d
yash7721 <77916043+yash7721@users.noreply.github.com>
2023-07-02 23:30:13 +0530

    Update Login.js

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -40,7 +40,7 @@ const Login = () => {
       };
 
       const { data } = await axios.post(
-        "/api/user/login",
+        "https://connectus.onrender.com/api/user/login",
         { email, password },
         config
       );
