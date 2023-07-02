9ac57908cf2773e36bbf39e3a1a9a0cfc4779d16
yash7721 <77916043+yash7721@users.noreply.github.com>
2023-07-02 23:29:26 +0530

    Update SingleChat.js

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -51,7 +51,7 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
       setLoading(true);
 
       const { data } = await axios.get(
-        `/api/message/${selectedChat._id}`,
+        `https://connectus.onrender.com/api/message/${selectedChat._id}`,
         config
       );
       setMessages(data);
@@ -82,7 +82,7 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
         };
         setNewMessage("");
         const { data } = await axios.post(
-          "/api/message",
+          "https://connectus.onrender.com/api/message",
           {
             content: newMessage,
             chatId: selectedChat,
