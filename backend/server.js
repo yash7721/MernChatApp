a32d8c99334d0c26c3d5f4898cbd3727bfbeabe9
yash7721 <77916043+yash7721@users.noreply.github.com>
2023-07-03 00:35:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -25,17 +25,17 @@ app.use("/api/message", messageRoutes);
 
 // const __dirname1 = path.resolve();
 
-// if ("production" === "production") {
-//   app.use(express.static(path.join(__dirname1, "/frontend/build")));
+if ("production" === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
-//   app.get("*", (req, res) =>
-//     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-//   );
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running..");
-//   });
-// }
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 // --------------------------deployment------------------------------
 
